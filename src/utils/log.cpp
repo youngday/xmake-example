d0adadb468cc81bef002dd7f46eb83057af0fa0b
youngday <young.day@aliyun.com>
2024-12-21 13:56:25 +0800

    log quill update  6.1.2 to 7.5.0,add FilenameAppendOption

--- src/utils/log.cpp
+++ src/utils/log.cpp
@@ -8,53 +8,49 @@ void quill_init();
 
 void quill_init() {
 
-  auto time_str = mylocal_time();
-
+  // auto time_str = mylocal_time();
   std::filesystem::create_directory("./log_quill/");
-  string filename = "log_quill/quill-" + time_str + ".log";
+  string filename = "log_quill/quill-.log";
 
-
- // Start the backend thread
+  // Start the backend thread
   quill::BackendOptions backend_options;
   quill::Backend::start(backend_options);
 
   // Console sink
   std::string console_log_pattern =
-    "[%(time)][%(log_level)][%(logger)][%(process_id)][%(thread_name)][%(file_name):%(line_number)]-%(message)";
+      "[%(time)][%(log_level)][%(logger)][%(process_id)][%(thread_name)][%("
+      "file_name):%(line_number)]-%(message)";
   std::string console_time_format = "%Y-%m-%d %H:%M:%S.%Qms";
   auto console_sink = quill::Frontend::create_or_get_sink<ConsoleSinkWithFormatter>(
-    "sink_id_1", console_log_pattern, console_time_format);
+    "sink_id_1", quill::PatternFormatterOptions{console_log_pattern, console_time_format});
   console_sink->set_log_level_filter(quill::LogLevel::Debug);
 
   // File sink
   std::string const file_log_pattern = "%(log_level);%(time);%(logger);%(message)";
   std::string const file_time_format = "%Y%m%dT%H:%M:%S.%Qus";
-
-  auto rotating_file_sink = quill::Frontend::create_or_get_sink<quill::RotatingFileSink>(
-    "rotating_file.log",
-    []()
-    {
-      // See RotatingFileSinkConfig for more options
-      quill::RotatingFileSinkConfig cfg;
-      cfg.set_open_mode('a');
-      cfg.set_max_backup_files(10);
-      cfg.set_rotation_time_daily("18:30");
-      cfg.set_rotation_max_file_size(1'000'000'000);
-      return cfg;
-    }());
+  auto rotating_file_sink =
+      quill::Frontend::create_or_get_sink<quill::RotatingFileSink>(
+          filename, []() {
+            // See RotatingFileSinkConfig for more options
+            quill::RotatingFileSinkConfig cfg;
+            cfg.set_filename_append_option(quill::FilenameAppendOption::StartCustomTimestampFormat, "%Y-%m-%d");
+            cfg.set_open_mode('a');
+            cfg.set_max_backup_files(10);
+            cfg.set_rotation_time_daily("18:30");
+            cfg.set_rotation_max_file_size(1'000'000'000);
+            return cfg;
+          }());
   rotating_file_sink->set_log_level_filter(quill::LogLevel::Debug);
 
   // The Logger is using the file_log_pattern by default
-  // To output our custom format to the file we use our own ConsoleSinkWithFormatter that is
-  // overwriting the default format
+  // To output our custom format to the file we use our own
+  // ConsoleSinkWithFormatter that is overwriting the default format
+
   logger = quill::Frontend::create_or_get_logger(
-    "logger", {std::move(console_sink), std::move(rotating_file_sink)}, file_log_pattern, file_time_format);
+      "logger", {std::move(console_sink), std::move(rotating_file_sink)},
+      quill::PatternFormatterOptions{file_log_pattern, file_time_format});
 
   logger->set_log_level(quill::LogLevel::Debug);
 
   LOG_INFO(logger, "This is a log info example {}", sizeof(std::string));
-
-
-
-
 }
